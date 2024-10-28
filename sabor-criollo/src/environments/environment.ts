c4af46a169d4bef7d0d3909916e1527bcdfa0620
joemafu <joe_mafu@hotmail.com>
2024-10-28 12:03:53 -0300

    Firebase

--- sabor-criollo/src/environments/environment.ts
+++ sabor-criollo/src/environments/environment.ts
@@ -6,6 +6,23 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+
+  apiKey: "AIzaSyCysdjdw0CIdjhtPRTqUMNtf0B3ZZvU38c",
+
+  authDomain: "sabor-criollo-2024.firebaseapp.com",
+
+  projectId: "sabor-criollo-2024",
+
+  storageBucket: "sabor-criollo-2024.appspot.com",
+
+  messagingSenderId: "56567677627",
+
+  appId: "1:56567677627:web:9d51be6dcf0ba3bbac5f9e"
+
+};
+
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
