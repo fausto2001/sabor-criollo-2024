7d9fceaeeaaf1650ebf82cc1daa183a25787199c
fausto2001 <faustopanello2017@gmail.com>
2024-11-14 12:52:00 -0300

    Avances encuesta

--- sabor-criollo/src/app/pages/encuesta/encuesta.page.ts
+++ sabor-criollo/src/app/pages/encuesta/encuesta.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { thumbsUpOutline, thumbsDownOutline } from 'ionicons/icons';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonRow, IonCol, IonItem, IonCardHeader, IonCard, IonLabel, IonCardTitle, IonCardContent, IonIcon } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-encuesta',
   templateUrl: './encuesta.page.html',
   styleUrls: ['./encuesta.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCard, IonCardHeader, IonCol, IonRow, IonImg,  IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, 
+    IonLabel, IonCardTitle, IonCardContent, IonItem, ReactiveFormsModule, IonIcon]
 })
 export class EncuestaPage implements OnInit {
 
-  constructor() { }
+  protected form: FormGroup;
+  puntaje: number = 1;
+
+  constructor(private router:Router)
+  {
+    addIcons({ //tuve que meterle esto para que funcionen los íconos de ionic
+      'thumbs-up-outline': thumbsUpOutline,
+      'thumbs-down-outline': thumbsDownOutline      
+    })
+
+    this.form = new FormGroup ({
+      
+    });
+  }
 
   ngOnInit() {
+  }
+
+  goHome(){
+    this.router.navigateByUrl('/home');
+  }
+
+  registrarEncuesta(){
+    
   }
 
 }
